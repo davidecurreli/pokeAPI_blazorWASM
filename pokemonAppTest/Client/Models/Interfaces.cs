ee2a579c8ab46f088d3aff8be19240a0e7850410
davidecurreli <davide.curreli95gmail.com>
2021-05-17 09:52:23 +0200

    first push. App currently working but just barely. TODO: isolate API calls, create an error-handling system

---
+++ pokemonAppTest/Client/Models/Interfaces.cs
@@ -0,0 +1,68 @@
+﻿namespace BlazorApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+    
+    public class PokemonFetchList
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public PokemonList[] Results { get; set; }
+    }
+    
+    public class PokemonList
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class PokemonDetails
+    {
+        public PokemonDetails() { }
+        public PokemonDetails(PokemonAbility[] abilities, string name, PokemonType[] types, int base_experience, int height, int weight)
+        {
+            Abilities = abilities;
+            Name = name;
+            Types = types;
+            Base_experience = base_experience;
+            Height = height;
+            Weight = weight;
+        }
+
+        public PokemonAbility[] Abilities { get; set; }
+        public string Name { get; set; }
+        public PokemonType[] Types { get; set; }
+        public int Base_experience { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class PokemonAbility
+    {
+        public int Slot { get; set; }
+        public Ability Ability { get; set; }
+    }
+
+    public class Ability
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class PokemonType
+    {
+        public int Slot { get; set; }
+        public Type Type { get; set; }
+    }
+
+    public class Type
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
+
